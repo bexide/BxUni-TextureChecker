7bc6c7fee480412df557877cd557277326e1b56e
HAYASHI Yozo <hayashi@bexide.co.jp>
2022-11-24 15:55:55 +0900

    Update:refactor;HierarchyPathクラスを分離

---
+++ Packages/TextureChecker/Editor/HierarchyPath.cs
@@ -0,0 +1,123 @@
+﻿// 2022-11-24 BeXide,Inc.
+// by Y.Hayashi
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace BX.TextureChecker
+{
+    /// <summary>
+    /// Hierarchy内のGameObjectの位置を示す情報
+    /// </summary>
+    public class HierarchyPath
+    {
+        public class NodeEntry
+        {
+            public string NodeName     { get; }
+            public int    SiblingIndex { get; }
+
+            public NodeEntry(Transform transform)
+            {
+                NodeName     = transform.name;
+                SiblingIndex = transform.GetSiblingIndex();
+            }
+
+            public bool IsMatch(Transform transform)
+            {
+                return NodeName == transform.name &&
+                       SiblingIndex == transform.GetSiblingIndex();
+            }
+        }
+
+        private NodeEntry[] NodeEntries { get; }
+
+        public HierarchyPath(IEnumerable<NodeEntry> entries)
+        {
+            NodeEntries = entries.ToArray();
+        }
+
+        public GameObject GetGameObject()
+        {
+            if (NodeEntries == null || NodeEntries.Length <= 0) { return null; }
+
+            Transform currentNode = null;
+            foreach (var entry in NodeEntries)
+            {
+                if (currentNode == null)
+                {
+                    var rootNode = GetRootNode(entry);
+                    Debug.Assert(
+                        rootNode != null,
+                        $"entry [{entry.NodeName}({entry.SiblingIndex})] cannot find in root");
+                    currentNode = rootNode.transform;
+                }
+                else
+                {
+                    currentNode = GetChildNode(currentNode, entry);
+                    Debug.Assert(currentNode != null);
+                }
+            }
+
+            if (currentNode == null) { return null; }
+
+            return currentNode.gameObject;
+        }
+
+        private GameObject GetRootNode(NodeEntry entry)
+        {
+            // ルートノード取得
+            var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            if (prefabStage == null)
+            {
+                // Sceneモード
+                for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
+                {
+                    var scene = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i);
+                    foreach (var rootGameObject in scene.GetRootGameObjects())
+                    {
+                        if (entry.IsMatch(rootGameObject.transform)) { return rootGameObject; }
+                    }
+                }
+            }
+            else
+            {
+                // Prefabモード
+                return prefabStage.prefabContentsRoot;
+            }
+            return null;
+        }
+
+        private Transform GetChildNode(Transform parent, NodeEntry entry)
+        {
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                var child = parent.GetChild(i);
+                if (entry.IsMatch(child)) { return child; }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 既存のGameObjectから新たに生成
+        /// </summary>
+        public static HierarchyPath Create(GameObject gameObject)
+        {
+            var ancestors = new List<NodeEntry>();
+            for (var trans = gameObject.transform; trans != null; trans = trans.parent)
+            {
+                ancestors.Add(new NodeEntry(trans));
+            }
+
+            return new HierarchyPath(ancestors.AsEnumerable().Reverse());
+        }
+
+        public override string ToString()
+        {
+            if (NodeEntries == null ||
+                NodeEntries.Length <= 0) { return string.Empty; }
+            return NodeEntries[^1].NodeName;
+        }
+    }
+}
--- Packages/TextureChecker/Editor/TextureCheckerBase.cs
+++ Packages/TextureChecker/Editor/TextureCheckerBase.cs
@@ -4,15 +4,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-
 using UnityEngine;
 using UnityEngine.U2D;
 
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEditor.U2D;
-using UnityEditor.SceneManagement;
 
 namespace BX.TextureChecker
 {
@@ -46,132 +43,6 @@ namespace BX.TextureChecker
 
         /// <summary>アトラスに登録されているテクスチャ一覧</summary>
         protected Dictionary<string, bool> AtlasedTextureMap { get; set; }
-
-        /// <summary>
-        /// Hierarchy内のGameObjectの位置を示す情報
-        /// </summary>
-        protected class HierarchyPath
-        {
-            public class NodeEntry
-            {
-                public string NodeName     { get; }
-                public int    SiblingIndex { get; }
-
-                public NodeEntry(Transform transform)
-                {
-                    NodeName     = transform.name;
-                    SiblingIndex = transform.GetSiblingIndex();
-                }
-
-                public bool IsMatch(Transform transform)
-                {
-                    return NodeName == transform.name &&
-                           SiblingIndex == transform.GetSiblingIndex();
-                }
-            }
-
-            private NodeEntry[] NodeEntries { get; }
-
-            public HierarchyPath(IEnumerable<NodeEntry> entries)
-            {
-                NodeEntries = entries.ToArray();
-            }
-
-            public GameObject GetGameObject()
-            {
-                if (NodeEntries == null || NodeEntries.Length <= 0)
-                {
-                    return null;
-                }
-
-                Transform currentNode = null;
-                foreach (var entry in NodeEntries)
-                {
-                    if (currentNode == null)
-                    {
-                        var rootNode = GetRootNode(entry);
-                        Debug.Assert(rootNode != null, $"entry [{entry.NodeName}({entry.SiblingIndex})] cannot find in root");
-                        currentNode = rootNode.transform;
-                    }
-                    else
-                    {
-                        currentNode = GetChildNode(currentNode, entry);
-                        Debug.Assert(currentNode != null);
-                    }
-                }
-
-                if (currentNode == null)
-                {
-                    return null;
-                }
-
-                return currentNode.gameObject;
-            }
-
-            private GameObject GetRootNode(NodeEntry entry)
-            {
-                // ルートノード取得
-                var prefabStage = PrefabStageUtility.GetCurrentPrefabStage(); 
-                if (prefabStage == null)
-                {
-                    // Sceneモード
-                    for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
-                    {
-                        var scene = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i);
-                        foreach (var rootGameObject in scene.GetRootGameObjects())
-                        {
-                            if (entry.IsMatch(rootGameObject.transform))
-                            {
-                                return rootGameObject;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    // Prefabモード
-                    return prefabStage.prefabContentsRoot;
-                }
-                return null;
-            }
-
-            private Transform GetChildNode(Transform parent, NodeEntry entry)
-            {
-                for (int i = 0; i < parent.childCount; i++)
-                {
-                    var child = parent.GetChild(i);
-                    if (entry.IsMatch(child))
-                    {
-                        return child;
-                    }
-                }
-                return null;
-            }
-
-            /// <summary>
-            /// 既存のGameObjectから新たに生成
-            /// </summary>
-            public static HierarchyPath Create(GameObject gameObject)
-            {
-                var ancestors = new List<NodeEntry>();
-                for (var trans = gameObject.transform; trans != null; trans = trans.parent)
-                {
-                    ancestors.Add(new NodeEntry(trans));
-                }
-
-                return new HierarchyPath(ancestors.AsEnumerable().Reverse());
-            }
-            
-            public override string ToString()
-            {
-                if (NodeEntries == null ||
-                    NodeEntries.Length <= 0)
-                {
-                    return string.Empty;
-                }
-                return NodeEntries[^1].NodeName;
-            }
-        }
 
         protected class InformationEntry
         {
